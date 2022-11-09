c8dc764f8a53103ad720598086549526e7c412f9
ismail <ickoca@gmail.com>
2022-11-09 15:57:42 +0300

    dortislem

---
+++ src/HelloWorld/C02_DortIslem.java
@@ -0,0 +1,19 @@
+package HelloWorld;
+
+public class C02_DortIslem {
+
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=5;
+
+        int toplam=a+b;
+        int carpim=a*b;
+        int bolum=a/b;
+        int fark=a-b;
+
+        System.out.println("a ve b'nin toplamı:"+toplam+"\n"+"a ve b'nin carpımı:"+carpim+"\n"+"a'nın b'ye bölümü:"+bolum+"\n"+"a ve b'nin farkı:"+fark);
+
+
+    }
+}
