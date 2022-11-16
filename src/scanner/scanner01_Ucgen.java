3bd00adc4c33125e2e511aa1dfa9d2ff5006be94
ismail <ickoca@gmail.com>
2022-11-16 15:19:00 +0300

    ScannerUcgen

---
+++ src/scanner/scanner01_Ucgen.java
@@ -0,0 +1,25 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class scanner01_Ucgen {
+    public static void main(String[] args) {
+        //soru:Kullanicidan aldiginiz sekille asagidaki gibi bir gorunum olusturan bir kod yaziniz.
+
+        /*
+        *         A
+        *        A A
+        *       A A A
+        * */
+
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("lütfen bir karakter giriniz");
+        char ch=scan.next().charAt(0);
+
+        System.out.println("  "+ch+"  ");
+        System.out.println(" "+ch+" "+ch+" ");
+        System.out.println(ch+" "+ch+" "+ch);
+    }
+
+}
